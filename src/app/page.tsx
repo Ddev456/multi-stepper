c297d2d680a61c7718c345b9d45b236de7d18191
Dylan DELHALLE <74488220+Ddev456@users.noreply.github.com>
2025-02-13 06:42:47 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import ApplicationForm from "@/components/applicationForm/form";
+import GardenPlannerForm from "@/components/gardenPlanner/GardenPlannerForm";
 
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto p-4">
-      <ApplicationForm />
+      <GardenPlannerForm />
     </div>
   )
 }
